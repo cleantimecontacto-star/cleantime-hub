2dd691c8ca69619ca7f04d32b936a1e778d95195
CleanTime Dev <dev@cleantime.local>
2026-05-04 20:35:26 -0400

    chore: bump service worker cache to v8 for reports module update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cleantime-v7';
+const CACHE_NAME = 'cleantime-v8';
 const urlsToCache = ['/', '/icon/icon-192.png', '/icon/icon-512.png'];
 
 self.addEventListener('install', (event) =>
