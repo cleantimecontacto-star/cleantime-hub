9b8240766d925fc293b660d1f4a99c97345c8be5
CleanTime Dev <dev@cleantime.local>
2026-05-04 20:54:50 -0400

    chore: bump cache to v12 for mobile UI improvements

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cleantime-v11';
+const CACHE_NAME = 'cleantime-v12';
 const urlsToCache = ['/', '/icon/icon-192.png', '/icon/icon-512.png'];
 
 self.addEventListener('install', (event) =>
